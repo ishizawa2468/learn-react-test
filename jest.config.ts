aab6c05220cd4bf4c51a62c3f8755778f759e3e7
Osamu Ishizawa <kiaydr@gmail.com>
2025-05-03 22:49:12 +0900

    Setup for jest, react/testing-library

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.app.json" }],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "<rootDir>/styleMock.js", // Mock CSS imports
+  },
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}", // カバレッジ対象のファイル
+    "!src/**/*.test.{ts,tsx}", // テストファイルを除外
+    "!src/setupTests.ts", // 初期設定ファイルを除外
+  ],
+  coverageDirectory: "coverage", // カバレッジレポートの出力先
+  coverageReporters: ["text", "html"], // レポート形式
+};
+
+export default config;
